5284fa1001863d548e1fb71d2b10ddc5f3e2e22a
Michał Sokołowski <michal.sokolowski95@gmail.com>
2019-04-06 14:27:44 +0200

    Add project files.

---
+++ homework/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace homework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose file and start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 230);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "E:\\file.csv";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(82, 112);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(65, 69);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(104, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Which page?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 303);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ homework/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+using iTextSharp.text.pdf.parser;
+using CsvHelper;
+using System.IO;
+
+
+namespace homework
+{
+ 
+    public partial class Form1 : Form
+    { 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        public void button1_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog dlg = new OpenFileDialog();
+            string path;
+            dlg.Filter = "PDF Files(*.pdf)|*.pdf|All files(*.*)|*.*";
+            int numer = 0;
+            numer = Convert.ToInt32(textBox2.Text);
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                path = dlg.FileName.ToString();
+
+
+                string strText = string.Empty;
+
+                string f = "";
+                int contract=0, rema=0, remb=0, remc=0, remd=0, shorty=0, longi=0, close=0;
+                try
+                {
+                    PdfReader reader = new PdfReader(path);
+                    for (int page = numer; page <= numer; page++)
+                    {
+                        ITextExtractionStrategy its = new iTextSharp.text.pdf.parser.LocationTextExtractionStrategy();
+                        string s = PdfTextExtractor.GetTextFromPage(reader, page, its);
+                        s = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(s)));
+                        strText = strText + s;
+
+
+
+                        string[] lines = s.Split('\n');
+                        int j=0;
+                        int temp = -2;
+                        int suma = 0;
+                        string a = "";
+                        string b = "";
+                        string c = "";
+                        string d = "";
+                        for (j=0; j<=lines.Length-3;j++)
+                        {
+                            
+                            
+
+                            if (lines[j].Contains("CONTRACT DESCRIPTION") == true)
+                            {
+                                //pobieranie nazwy instrumentu a
+                                
+                                contract = lines[j].IndexOf("CONTRACT");
+                                shorty = lines[j].IndexOf("SHORT");
+                                longi = lines[j].IndexOf("LONG");
+                                close = lines[j].IndexOf("PRICE");
+
+                                
+
+                                temp = j;
+                            }
+
+                            // if (j == temp + 1)
+                            // {
+                            //  while (suma != 2) 
+                            // {
+                            //   for (int k=5; k<=lines.Length-3; k++) {
+
+                            a = lines[j].Substring(contract);
+                            rema = a.IndexOf("   ");
+                            a = a.Remove(rema);
+
+                            if (lines[j].Contains(a) == true && lines[j + 1].Contains("SPREAD") == true)
+                                        {
+                                        do
+                                        {
+                                            b = lines[j + 2].Substring(shorty);
+                                            remb = b.IndexOf("*");
+                                            b = b.Remove(contract);
+                                            if (b.Contains("*") == true)
+                                            { b = b.Remove(remb); }
+                                            else { b = ""; }
+
+                                            c = lines[j + 2].Substring(longi);
+                                            remc = c.IndexOf("*");
+                                            c = c.Remove(shorty-16);
+                                            if (c.Contains("*") == true)
+                                            { c = c.Remove(remc); }
+                                            else { c = ""; }
+
+                                            d = lines[j + 2].Substring(close);
+                                            remd = d.IndexOf("   ");
+                                            d = d.Remove(remd);
+                                            suma = 2;
+
+                                           // MessageBox.Show(a);
+                                            
+                                        } while (suma != 2);
+                                        
+                                            
+    
+                                        }
+                                        else
+                                        {
+                                            //MessageBox.Show("tooo");
+                                        }
+
+                            // }
+
+                            // } 
+                            //}
+                            if(a!="" && d != "" && (b !="" || c!="") ) { 
+                            f = f + a + ";" + c + ";" + b + ";" + d + ";" + '\n';
+                                a = "";
+                                b = "";
+                                c = "";
+                                d = "";
+                            }
+                        }
+                        string g;
+                        g = "Contract Description;LONG;SHORT;CLOSE;";
+                        listBox1.Items.Add(g);
+                        listBox1.Items.Add(f);
+                       
+                      //  System.IO.File.WriteAllLines(@"C:\Users\michal\desktop\WriteLines.txt", lines);
+
+
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string eksport;
+            eksport = textBox1.Text;
+
+            StreamWriter MyoutputWriter = new StreamWriter(@eksport);
+            foreach (var item in listBox1.Items)
+            {
+                MyoutputWriter.WriteLine(item.ToString());
+            }
+            MyoutputWriter.Close();
+            listBox1.Items.Clear();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
